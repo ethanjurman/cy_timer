8b32e5de148f36683811ebb36e36b0f0d514e98e
Ethan Jurman <ethanjurman@gmail.com>
2023-04-09 19:26:13 -0400

    timer done

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let timers = JSON.parse(localStorage.getItem('timers')) || [];
 let isPaused = false;
 let pauseTime = null;
+let isEditingText = false;
 
 function createTimer(timeMS) {
   const timerWrapper = document.createElement('div');
@@ -13,18 +14,46 @@ function createTimer(timeMS) {
   const timerDrain = document.createElement('div');
   timerDrain.classList.add('timer-drain');
 
+  const timerName = document.createElement('div');
+  timerName.classList.add('timer-name');
+  timerName.setAttribute('data-content', timers.length.toString().padStart(2, '0'));
+
+  timerWrapper.appendChild(timerName);
   timerWrapper.appendChild(timerElement);
   timerWrapper.appendChild(timerDrain);
-  document.body.appendChild(timerWrapper);
+  document.querySelector('.timers-container').appendChild(timerWrapper);
+
+  timerName.onclick = (event) => {
+    if (isEditingText) {
+      isEditingText = false;
+      timerName.classList.remove('text-edit');
+      return;
+    }
+
+    isEditingText = true;
+    timerName.classList.add('text-edit');
+    event.preventDefault();
+  }
 
   timerWrapper.onclick = (event) => {
     let element = event.target;
+    if (element.classList.contains('timer-name') || isEditingText) {
+      return;
+    }
     if (!element.classList.contains('timer-wrapper')) {
       element = event.target.parentElement;
     }
     index = element.getAttribute('data-index');
     timers.splice(index, 1);
     element.parentElement.removeChild(element);
+
+    // update other timers by subtracting all of them by 1 if they were above the index
+    document.querySelectorAll('.timer-wrapper').forEach(timerElement => {
+      const currentIndex = Number(timerElement.getAttribute('data-index'));
+      if (currentIndex > index) {
+        timerElement.setAttribute('data-index', currentIndex - 1)
+      }
+    })
   }
 
   const nowTime = new Date().getTime();
@@ -70,13 +99,11 @@ function updateTimers() {
 }
 
 function pauseTimers() {
-  console.log('resuming timers');
   isPaused = true;
   pauseTime = new Date().getTime();
 }
 
 function resumeTimers() {
-  console.log('resuming timers');
   const resumeTime = new Date().getTime();
   timers = timers.map((timer) => {
     return {
@@ -97,20 +124,34 @@ function divideTimers() {
 
 function shrinkTimers(multiplier = 1) {
   timers.forEach((timer, index) => {
-    timers[index].endTime -= 100 * multiplier;
-    timers[index].duration -= 100 * multiplier;
+    const factor = timers[index].duration / 100;
+
+    timers[index].endTime -= factor * multiplier;
+    timers[index].duration -= factor * multiplier;
   })
 }
 
 function extendTimers(multiplier = 1) {
   timers.forEach((timer, index) => {
-    timers[index].endTime += 100 * multiplier;
-    timers[index].duration += 100 * multiplier;
+    const factor = timers[index].duration / 100;
+
+    timers[index].endTime += factor * multiplier;
+    timers[index].duration += factor * multiplier;
+  })
+}
+
+function deleteTimers() {
+  timers = [];
+  document.querySelectorAll('.timer-wrapper').forEach(timerElement => {
+    timerElement.parentElement.removeChild(timerElement);
   })
 }
 
 addEventListener('keydown', (event) => {
   console.log(event.key);
+  if (isEditingText) {
+    return updateTimerTitle(event.key);
+  }
   if (event.key === ']') {
     return extendTimers();
   }
@@ -132,7 +173,45 @@ addEventListener('keydown', (event) => {
   if (event.key === 'Enter' && isPaused) {
     return resumeTimers();
   }
+  if (event.key === 'Escape') {
+    return deleteTimers();
+  }
+  if (event.key === 'h') {
+    if (document.querySelector('.button-container').style.display === 'none') {
+      document.querySelector('.button-container').style.display = 'flex';
+    } else {
+      document.querySelector('.button-container').style.display = 'none';
+    }
+  }
+  if (event.key === '1') { createTimer(60000) }
+  if (event.key === '2') { createTimer(60000 * 5) }
+  if (event.key === '3') { createTimer(60000 * 10) }
+  if (event.key === '4') { createTimer(60000 * 30) }
+  if (event.key === '5') { createTimer(60000 * 60) }
+  if (event.key === '6') { createTimer(60000 * 60 * 4) }
+  if (event.key === '7') { createTimer(60000 * 60 * 8) }
+  if (event.key === '8') { createTimer(60000 * 60 * 16) }
 })
+
+function updateTimerTitle(key) {
+  const editTextElement = document.querySelector('.text-edit');
+  const text = editTextElement.getAttribute('data-content');
+  if (key === 'Backspace') {
+    return editTextElement.setAttribute('data-content', text.slice(0, text.length - 1));
+  }
+  if (key === 'Enter') {
+    isEditingText = false;
+    editTextElement.classList.remove('text-edit');
+  }
+  if (key === ' ') {
+    return editTextElement.setAttribute('data-content', text + '_');
+  }
+  if (key.length > 1) {
+    return; // ignore special characters and strings
+  }
+  return editTextElement.setAttribute('data-content', text + key);
+
+}
 
 document.querySelector('.minute-1-timer').onclick = () => createTimer(60000)
 document.querySelector('.minute-5-timer').onclick = () => createTimer(60000 * 5)
@@ -141,5 +220,6 @@ document.querySelector('.minute-30-timer').onclick = () => createTimer(60000 * 3
 document.querySelector('.hour-1-timer').onclick = () => createTimer(60000 * 60)
 document.querySelector('.hour-4-timer').onclick = () => createTimer(60000 * 60 * 4)
 document.querySelector('.hour-8-timer').onclick = () => createTimer(60000 * 60 * 8)
+document.querySelector('.hour-16-timer').onclick = () => createTimer(60000 * 60 * 16)
 
 window.requestAnimationFrame(updateTimers)
